88572841c99b0325a4a557870c5fa8b34e8e6239
Md-Shahriar-Rahman-Shihab <shihabshahriar21821@gmail.com>
2025-12-11 10:38:48 +0600

    create first user using post method

--- src/server.ts
+++ src/server.ts
@@ -56,8 +56,28 @@ app.get('/', (req:Request, res:Response) => {
   res.send('Hello Shihab Shahriar!')
 })
 
-app.post("/", (req:Request,res:Response)=>{
-    console.log(req.body);
+app.post("/users", async (req:Request,res:Response)=>{
+    // console.log(req.body);
+    const {name,email} = req.body;
+
+    try {
+        const result = await pool.query(`INSERT INTO users(name,email) VALUES($1,$2) RETURNING *`,[name,email]);
+        // console.log(result.rows[0]);
+        // res.send({message:"data inserted"})
+        res.status(201).json({
+            success: false,
+            message: "Data Inserted Successfully",
+            data: result.rows[0],
+        })
+        
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+
+
 
     res.status(201).json({
         success: true,
