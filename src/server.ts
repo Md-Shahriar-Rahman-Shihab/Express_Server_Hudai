63aa0b3a6c1dd51ab2f6c2ccbb61e5e732f1200d
Md-Shahriar-Rahman-Shihab <shihabshahriar21821@gmail.com>
2025-12-11 13:11:29 +0600

    404 route not found

--- src/server.ts
+++ src/server.ts
@@ -228,8 +228,13 @@ app.get("/todos",async (req: Request,res: Response)=>{
 })
 
 
-
-
+app.use((req: Request, res: Response)=>{
+    res.status(404).json({
+        success: false,
+        message: "Route not found",
+        path: req.path,
+    })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
